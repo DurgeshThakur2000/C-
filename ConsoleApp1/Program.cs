7f5d45d6973fc720e15c63d91dc9c3c62b7bb627
DurgeshThakur2000 <durgthakur11@gmail.com>
2024-03-07 23:53:59 +0530

    First commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string firstFriend = "          Durgesh     ";
+firstFriend = firstFriend.Trim();
+string secondFriend = "     Divyesh     ";
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend.Trim()}");
